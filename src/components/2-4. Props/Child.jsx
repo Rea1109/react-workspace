d7a1c0b066143ded8529948b9f44949689fe0ab5
rea <woohr1109@gmail.com>
2022-08-26 19:12:06 +0900

    [modify] props chapter

---
+++ src/components/2-4. Props/Child.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line react/prop-types
+export const Child = ({ name, age }) => {
+    return (
+        <div>
+            <span>{name}</span>
+            <span>{age}</span>
+        </div>
+    );
+};
